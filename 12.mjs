b74becc6e039a933abaabeb212359a7a0f2d2c97
Antoine du Hamel <duhamelantoine1995@gmail.com>
2022-12-06 09:25:38 +0100

    fixup! day 6

--- 12.mjs
+++ 12.mjs
@@ -2,23 +2,21 @@ import { open } from "node:fs/promises";
 
 const fh = await open(new URL("./06.txt", import.meta.url));
 
-const checkBuffer = (buf) => new Set(buf).size === 14;
-
 const buffer = new Uint8Array(14);
-let { bytesRead } = await fh.read({ buffer, length: 14 });
+let { bytesRead } = await fh.read({ buffer });
 let position = bytesRead;
-do {
-  if (checkBuffer(buffer)) {
+while (bytesRead !== 0) {
+  if (new Set(buffer).size === buffer.byteLength) {
     console.log(position);
     process.exit();
   }
   ({ bytesRead } = await fh.read({
     buffer,
     length: 1,
-    offset: position % 14,
+    offset: position % buffer.byteLength,
     position,
   }));
   position += bytesRead;
-} while (bytesRead !== 0);
+}
 
 console.log("No start-of-packet marker found");
